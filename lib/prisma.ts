d768262398617cc5b982946592e8c4f1431aa9cb
trung-1-cshap <trunggau18@gmail.com>
2025-12-27 09:48:50 +0700

    fix prisma singleton for vercel runtime

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,16 +2,12 @@ import { PrismaClient } from "@prisma/client";
 
 declare global {
   // eslint-disable-next-line no-var
-  var __prisma: PrismaClient | undefined;
+  var prisma: PrismaClient | undefined;
 }
 
-/**
- * KHÔNG khởi tạo Prisma ngay khi build
- * Chỉ khởi tạo khi runtime thực sự cần
- */
 export function getPrisma() {
-  if (!global.__prisma) {
-    global.__prisma = new PrismaClient();
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
   }
-  return global.__prisma;
+  return global.prisma;
 }
